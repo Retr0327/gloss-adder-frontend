d753c48aeb73612202343b2f5ee8718d0844913c
Retr0327 <philcoke35@gmail.com>
2022-04-07 16:54:06 +0800

    Update GlossAdderForm

--- src/containers/GlossAdderForm.tsx
+++ src/containers/GlossAdderForm.tsx
@@ -1,12 +1,38 @@
+import { Formik, Form, FormikHelpers } from "formik";
 import { Text, Title, Grid } from "@mantine/core";
 
+type ValueType = {
+  fileUpload: FormData | null;
+  clitic: string;
+};
+
 function GlossAdderForm() {
+  const initialValues: ValueType = {
+    fileUpload: null,
+    clitic: "",
+  };
+
+  const onSubmit = async (
+    values: ValueType,
+    actions: FormikHelpers<ValueType>
+  ) => {
+    console.log(values);
+  };
+
   return (
-    <Text size="xl" weight={500}>
-      <Title order={1} align="center">
-        Sample Form
-      </Title>
-    </Text>
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {(formik) => (
+        <Form>
+          <Grid justify="center" gutter="xl">
+            <Grid.Col xs={12} sm={12} md={12} lg={10}>
+              <Title order={1} align="center">
+                Gloss Adder
+              </Title>
+            </Grid.Col>
+          </Grid>
+        </Form>
+      )}
+    </Formik>
   );
 }
 

4d8c81410f1dbdd6efb4bcbe640bcc64be44f4ad
Retr0327 <philcoke35@gmail.com>
2022-04-07 21:49:26 +0800

    Update FormikController

--- src/components/FormikController/FormikComponents/FileDropZone.tsx
+++ src/components/FormikController/FormikComponents/FileDropZone.tsx
@@ -1,11 +1,13 @@
-import { Dropzone } from "@mantine/dropzone";
+import { Dropzone, DropzoneProps } from "@mantine/dropzone";
 import { useMantineTheme } from "@mantine/core";
 import FileDropzoneChildren from "./Helpers/FileDropZoneTools";
-import { SetFieldValuesProps, ValueType } from "src/typings";
+import { SetFieldValuesProps, ValueType, ControlledProps } from "src/typings";
 
 const TXT_MIME_TYPE = ["text/plain"];
 
-function FileDropZone({ setFieldValue }: SetFieldValuesProps<ValueType>) {
+function FileDropZone(props: DropzoneProps & ControlledProps) {
+
+  // { setFieldValue }: SetFieldValuesProps<ValueType>
   const theme = useMantineTheme();
 
   return (
--- src/components/FormikController/FormikController.tsx
+++ src/components/FormikController/FormikController.tsx
@@ -1,5 +1,15 @@
-function FormikController() {
-  return <div>FormikController</div>;
+import { ControllerProps } from "src/typings";
+import FileDropZone from "./FormikComponents/FileDropZone";
+
+function FormikController(props: ControllerProps) {
+  const { control, ...rest } = props;
+
+  switch (control) {
+    case "drop-zone":
+      return <FileDropZone {...rest} />;
+    default:
+      return null;
+  }
 }
 
 export default FormikController;

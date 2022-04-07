97b0704b46a77088680176ab9d8104d4bb0ac4b1
Retr0327 <philcoke35@gmail.com>
2022-04-07 22:01:41 +0800

    Add CustomFormik

--- src/components/FormikController/FormikComponents/FileDropZone.tsx
+++ src/components/FormikController/FormikComponents/FileDropZone.tsx
@@ -1,21 +1,29 @@
-import { Dropzone, DropzoneProps } from "@mantine/dropzone";
+import useCustomFormik from "./CustomFormik";
 import { useMantineTheme } from "@mantine/core";
+import { Dropzone, DropzoneProps } from "@mantine/dropzone";
 import FileDropzoneChildren from "./Helpers/FileDropZoneTools";
 import { SetFieldValuesProps, ValueType, ControlledProps } from "src/typings";
 
 const TXT_MIME_TYPE = ["text/plain"];
+const theme = useMantineTheme();
 
+// { setFieldValue }: SetFieldValuesProps<ValueType>
 function FileDropZone(props: DropzoneProps & ControlledProps) {
+  const { label, name, ...rest } = props;
+  const [formik, hasError] = useCustomFormik(name);
 
-  // { setFieldValue }: SetFieldValuesProps<ValueType>
-  const theme = useMantineTheme();
+  const handleOnDrop = (files: File[]) => {
+    console.log("accepted files", files);
+    formik.setFieldValue("fileUpload", files);
+  };
 
   return (
     <Dropzone
-      onDrop={(files) => console.log("accepted files", files)}
+      {...rest}
       onReject={(files) => console.log("rejected files", files)}
       maxSize={3 * 1024 ** 2}
       accept={TXT_MIME_TYPE}
+      onDrop={handleOnDrop}
     >
       {(status) => {
         return FileDropzoneChildren(status, theme);

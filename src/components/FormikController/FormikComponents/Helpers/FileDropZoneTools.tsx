358339ca9e1afd718e03c866f61fffa3cda21657
Retr0327 <philcoke35@gmail.com>
2022-04-07 20:46:37 +0800

    Rename to fileDropzoneChildren

--- src/components/FormikController/FormikComponents/Helpers/FileDropZoneTools.tsx
+++ src/components/FormikController/FormikComponents/Helpers/FileDropZoneTools.tsx
@@ -27,7 +27,7 @@ function ImageUploadIcon({
   return <FileImport {...props} />;
 }
 
-function FileDropzoneChildren(status: DropzoneStatus, theme: MantineTheme) {
+function fileDropzoneChildren(status: DropzoneStatus, theme: MantineTheme) {
   return (
     <Group
       position="center"
@@ -52,4 +52,4 @@ function FileDropzoneChildren(status: DropzoneStatus, theme: MantineTheme) {
   );
 }
 
-export default FileDropzoneChildren;
+export default fileDropzoneChildren;

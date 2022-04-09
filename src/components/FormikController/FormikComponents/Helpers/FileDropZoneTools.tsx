33913fa83c69aef38626af6d3af4edfc5a1d2913
Retr0327 <philcoke35@gmail.com>
2022-04-09 17:00:02 +0800

    Set no file restrictions

--- src/components/FormikController/FormikComponents/Helpers/FileDropZoneTools.tsx
+++ src/components/FormikController/FormikComponents/Helpers/FileDropZoneTools.tsx
@@ -2,10 +2,14 @@ import { DropzoneStatus } from "@mantine/dropzone";
 import { Group, Text, MantineTheme } from "@mantine/core";
 import { Upload, FileImport, X, Icon as TablerIcon } from "tabler-icons-react";
 
-function getIconColor(status: DropzoneStatus, theme: MantineTheme) {
+function getIconColor(
+  status: DropzoneStatus,
+  theme: MantineTheme,
+  hasError: JSX.Element | null
+) {
   return status.accepted
     ? theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6]
-    : status.rejected
+    : status.rejected || hasError
     ? theme.colors.red[theme.colorScheme === "dark" ? 4 : 6]
     : theme.colorScheme === "dark"
     ? theme.colors.dark[0]
@@ -14,20 +18,28 @@ function getIconColor(status: DropzoneStatus, theme: MantineTheme) {
 
 function ImageUploadIcon({
   status,
+  error,
   ...props
-}: React.ComponentProps<TablerIcon> & { status: DropzoneStatus }) {
+}: React.ComponentProps<TablerIcon> & {
+  status: DropzoneStatus;
+  error: JSX.Element | null;
+}) {
   if (status.accepted) {
     return <Upload {...props} />;
   }
 
-  if (status.rejected) {
+  if (status.rejected || error) {
     return <X {...props} />;
   }
 
   return <FileImport {...props} />;
 }
 
-function fileDropzoneChildren(status: DropzoneStatus, theme: MantineTheme) {
+function fileDropzoneChildren(
+  status: DropzoneStatus,
+  theme: MantineTheme,
+  hasError: JSX.Element | null
+) {
   return (
     <Group
       position="center"
@@ -36,16 +48,20 @@ function fileDropzoneChildren(status: DropzoneStatus, theme: MantineTheme) {
     >
       <ImageUploadIcon
         status={status}
-        style={{ color: getIconColor(status, theme) }}
+        error={hasError}
+        style={{ color: getIconColor(status, theme, hasError) }}
         size={80}
       />
 
       <div>
         <Text size="xl" inline>
-          Drag files here or click to select files
+          {hasError
+            ? "Please Select a File"
+            : "Drag files here or click to select files"}
         </Text>
         <Text size="sm" color="dimmed" inline mt={7}>
-          Attach as many files as you like, each file should not exceed 5mb
+          {!hasError &&
+            "Attach as many files as you like, each file should not exceed 5mb"}
         </Text>
       </div>
     </Group>

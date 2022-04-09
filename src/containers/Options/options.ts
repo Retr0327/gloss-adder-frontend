46ef2a180f181ce0d1ecad94cbdc85c21c31c01a
Retr0327 <philcoke35@gmail.com>
2022-04-09 16:24:48 +0800

    Fix show value bug

--- src/containers/Options/options.ts
+++ src/containers/Options/options.ts
@@ -1,12 +1,12 @@
 export const options = [
   {
     label: "分開",
-    value: 0,
+    value: "0",
     description: "將人稱代名詞與被依附詞分開",
   },
   {
     label: "不分開",
-    value: 1,
+    value: "1",
     description: "人稱代名詞附著於前面的被附著詞",
   },
 ];

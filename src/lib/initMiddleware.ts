f38bf941ac1c7d2b87d361580bcf538ac2f7eb99
Retr0327 <philcoke35@gmail.com>
2022-04-09 22:41:47 +0800

    Add initMiddleware

---
+++ src/lib/initMiddleware.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import cors, { CorsOptions, CorsOptionsDelegate } from "cors";
+
+function initMiddleware(middleware: typeof cors) {
+  const func = (
+    req: NextApiRequest,
+    res: NextApiResponse,
+    options?: CorsOptions | CorsOptionsDelegate
+  ) =>
+    new Promise((resolve, reject) => {
+      middleware(options)(req, res, (result: Error | unknown) => {
+        if (result instanceof Error) {
+          return reject(result);
+        }
+
+        return resolve(result);
+      });
+    });
+
+  return func;
+}
+
+const customCors = initMiddleware(cors);
+
+export default customCors;

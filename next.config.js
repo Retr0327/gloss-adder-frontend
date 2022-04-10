d42b155b2adbb9b68d112adf4945fba1b8678d5c
Retr0327 <philcoke35@gmail.com>
2022-04-10 17:42:20 +0800

    Add securityHeaders

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const ContentSecurityPolicy = `
+ default-src 'self';
+ script-src 'self' 'unsafe-eval' 'unsafe-inline';
+ style-src 'self' 'unsafe-inline' *.googleapis.com;
+ child-src *.google.com;
+ media-src 'none';
+ connect-src 'self';
+ font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+  {
+    key: "Permissions-Policy",
+    value: "camera=(), microphone=(), geolocation=()",
+  },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains",
+  },
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\n/g, ""),
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [{ source: "/:path*{/}?", headers: securityHeaders }];
+  },
+};

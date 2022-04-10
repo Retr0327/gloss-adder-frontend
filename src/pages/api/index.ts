58627d7f48ed11731c8505d024cd00cd4593c8f5
Retr0327 <philcoke35@gmail.com>
2022-04-10 19:33:18 +0800

    Update export style

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -5,11 +5,13 @@ type ResponseData = {
   ip: string | string[] | undefined;
 };
 
-export default async function handler(
+const handler = async (
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
-) {
+) => {
   const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
   return res.status(200).json({ status: "success", ip });
-}
+};
+
+export default handler;

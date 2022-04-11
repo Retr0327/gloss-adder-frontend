b618de5d8e93e5c544a6a69ea39d22daaf64ce35
Retr0327 <philcoke35@gmail.com>
2022-04-11 21:53:20 +0800

    Update handleUploadFile

--- src/pages/api/uploadGloss/index.ts
+++ src/pages/api/uploadGloss/index.ts
@@ -1,8 +1,19 @@
+import parseFile from "src/server/helpers/parseFile";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { checkAuthorized } from "src/server/middleware/index";
 
 const handleUploadFile = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  const result: any = await parseFile(req);
+  console.log("first", result);
+
   return res.status(200).json({ msg: "123" });
 };
 
 export default checkAuthorized(handleUploadFile);
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};

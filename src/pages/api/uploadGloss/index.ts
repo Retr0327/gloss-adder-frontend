43101ea00b83fab52291f84321e96d8e36205d80
Retr0327 <philcoke35@gmail.com>
2022-04-11 19:50:00 +0800

    Add checkAuthorized

--- src/pages/api/uploadGloss/index.ts
+++ src/pages/api/uploadGloss/index.ts
@@ -1,13 +1,8 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import checkAuthorized from "src/server/middleware/checkAuthorized";
 
 const handleUploadFile = async (req: NextApiRequest, res: NextApiResponse) => {
-  const apiKey = process.env.API_ROUTE_SECRET;
-
-  if (req.query.API_ROUTE_SECRET !== apiKey) {
-    return res.status(404).json({ message: "not authorized" });
-  }
-
   return res.status(200).json({ msg: "123" });
 };
 
-export default handleUploadFile;
+export default checkAuthorized(handleUploadFile);

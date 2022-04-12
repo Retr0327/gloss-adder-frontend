8a6ccac9a9173029719c8c44fce088eec18e51e4
Retr0327 <philcoke35@gmail.com>
2022-04-12 19:33:02 +0800

    Update upload path

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,6 +1,6 @@
 import path from "path";
 
-const UPLOAD_DIR = path.resolve("src", "api", "upload");
+const UPLOAD_DIR = path.resolve("src", "upload");
 const getUploadFileDir = (name: string) => path.resolve(UPLOAD_DIR, name);
 
 export { UPLOAD_DIR, getUploadFileDir };

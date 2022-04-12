a80936e1afe72a5eaca329f0d023e4c757dd498a
Retr0327 <philcoke35@gmail.com>
2022-04-12 19:37:04 +0800

    Check upload folder

--- src/pages/api/uploadGloss/index.ts
+++ src/pages/api/uploadGloss/index.ts
@@ -1,8 +1,14 @@
+import fs from "fs";
+import { UPLOAD_DIR } from "src/constants/constants";
 import parseFile from "src/server/helpers/parseFile";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { checkAuthorized } from "src/server/middleware/index";
 
 const handleUploadFile = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (!fs.existsSync(UPLOAD_DIR)) {
+    fs.mkdirSync(UPLOAD_DIR);
+  }
+
   const result = await parseFile(req);
   console.log("first", result);
 

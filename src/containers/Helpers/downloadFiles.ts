97cdde9bff0fa3f521814077c9dd7366f137f07f
Retr0327 <philcoke35@gmail.com>
2022-04-15 21:43:02 +0800

    Add downFiles

---
+++ src/containers/Helpers/downloadFiles.ts
@@ -0,0 +1,12 @@
+function saveFile(blob: Blob, fileName: string) {
+  const url = window.URL.createObjectURL(blob);
+  const anchor = document.createElement("a");
+  anchor.style.display = "none";
+  anchor.href = url;
+  anchor.download = fileName;
+  document.body.appendChild(anchor);
+  anchor.click();
+  return window.URL.revokeObjectURL(url);
+}
+
+export default saveFile;

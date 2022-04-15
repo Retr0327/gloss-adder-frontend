8b70f1def0f467cc668cbd236bb622bda3211de5
Retr0327 <philcoke35@gmail.com>
2022-04-15 19:46:00 +0800

    Add onModifyGlossClick

--- src/components/ButtonController.tsx
+++ src/components/ButtonController.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@mantine/core";
+import { useRouter } from "next/router";
 import { IconUpload, IconClick } from "@tabler/icons";
 
 type ButtonControllerType = {
@@ -6,9 +7,19 @@ type ButtonControllerType = {
 };
 
 function ButtonController({ control }: ButtonControllerType) {
+  const router = useRouter();
+
+  const onModifyGlossClick = async () => {
+    const { token, cliticOption } = router.query;
+  };
+
   switch (control) {
     case "modify":
-      return <Button leftIcon={<IconClick />}>新增第三行</Button>;
+      return (
+        <Button leftIcon={<IconClick />} onClick={onModifyGlossClick}>
+          新增第三行
+        </Button>
+      );
     default:
       return (
         <Button type="submit" leftIcon={<IconUpload />}>

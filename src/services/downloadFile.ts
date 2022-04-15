f93aad4bc6ffa94fc670fed631cfe234da5edc75
Retr0327 <philcoke35@gmail.com>
2022-04-15 20:55:54 +0800

    Add downloadFile

---
+++ src/services/downloadFile.ts
@@ -0,0 +1,18 @@
+import customFetch from "./customFetch";
+
+async function downloadFile(token: string) {
+  const url = `${process.env.NEXT_PUBLIC_SERVER_URL}/download/${token}`;
+  try {
+    const result = await customFetch({
+      method: "GET",
+      url,
+      purpose: "download",
+    });
+    return [result, null];
+  } catch (error) {
+    console.error(error);
+    return [null, error];
+  }
+}
+
+export default downloadFile;

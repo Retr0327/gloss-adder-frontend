4cc4f35b112ac42a31c5fdd81eda25e7c098b056
Retr0327 <philcoke35@gmail.com>
2022-04-15 20:48:20 +0800

    Update index

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 import ProgressBar from "./ProgressBar";
+import SelectedFiles from "./SelectedFiles";
+import ButtonController from "./ButtonController";
 import FormikController from "./FormikController/FormikController";
 
 export { FormikController, ProgressBar };

7ff65e32de4508af5044d8028bd7f7230e4a7681
Retr0327 <philcoke35@gmail.com>
2022-04-15 19:14:21 +0800

    Add ButtonController

---
+++ src/components/ButtonController.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@mantine/core";
+import { IconUpload } from "@tabler/icons";
+
+type ButtonControllerType = {
+  control?: string;
+};
+
+function ButtonController({ control }: ButtonControllerType) {
+  switch (control) {
+    case "modify":
+      return null;
+    default:
+      return (
+        <Button type="submit" leftIcon={<IconUpload />}>
+          上傳
+        </Button>
+      );
+  }
+}
+
+export default ButtonController;

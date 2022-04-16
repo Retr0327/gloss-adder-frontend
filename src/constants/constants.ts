7ab13a8967aeff4927b8664275ee7d2bb17d7608
Retr0327 <philcoke35@gmail.com>
2022-04-16 15:33:48 +0800

    Remove unused scripts

--- src/constants/constants.ts
+++
@@ -1,6 +0,0 @@
-import path from "path";
-
-const UPLOAD_DIR = path.resolve("src", "upload");
-const getUploadedFileDir = (name: string) => path.resolve(UPLOAD_DIR, name);
-
-export { UPLOAD_DIR, getUploadedFileDir };

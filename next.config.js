b64a4e4e46dbdca4fdec781765d215208fdf3453
Retr0327 <philcoke35@gmail.com>
2022-04-17 22:30:06 +0800

    Config next

--- next.config.js
+++ next.config.js
@@ -51,9 +51,6 @@ const nextConfig = {
   async headers() {
     return [{ source: "/:path*{/}?", headers: securityHeaders }];
   },
-  experimental: {
-    outputStandalone: true,
-  },
 };
 
 module.exports = nextConfig;

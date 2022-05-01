4671e1e1b993843bb11fbe36696464b56d7cbd22
Retr0327 <philcoke35@gmail.com>
2022-05-01 19:44:25 +0800

    Update constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,12 @@
-export const DISABLED = "DISABLED";
-export const RESET = "RESET";
+const DISABLED = "DISABLED";
+const RESET = "RESET";
+
+const isProduction: boolean = process.env.NODE_ENV === "production";
+
+const API: string = !isProduction ? "http://localhost:3000" : "/api";
+
+const EXTERNAL_API: string = !isProduction
+  ? "http://localhost:3000"
+  : `http://api:${process.env.API_PORT}`;
+
+export { API, EXTERNAL_API, DISABLED, RESET };
